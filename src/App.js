2bf5ad339c03d2aebb36b4a44bbe5690b031a2b4
hebatullahgassem <hebatullahgassem@gmail.com>
2024-02-20 22:14:31 +0200

    one

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import List from './App.module.css';
+import Card from './component/Card';
+
+function App() {
+  return (
+    <>
+    <div className={List.cardsList}>
+      <Card>{['01', 'Where are these chairs assembled? ']}</Card>
+      <Card>{['02', 'How long do I have to return my chair?']}</Card>
+      <Card>{['03', 'Do you ship to countries outside the EU?']}</Card>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Card.js
@@ -0,0 +1,26 @@
+import './Card.css';
+import React, {useState} from 'react';
+
+export default function  Card ({children}){
+  const [isOpen, setIsOpen] = useState(false);
+  const [className, setClassName] = useState('card');
+
+  function displayContent (e){
+    //toggle
+    setIsOpen(!isOpen);
+    {isOpen ? setClassName('card') : setClassName('card2')}
+  }
+
+  return(
+    <>
+    <div className={className} onClick={displayContent}>
+      <div className='head'>
+      <p className="number">{children[0]}</p> 
+      <h1 className="title">{children[1]}</h1>
+      <button onClick={displayContent}>{isOpen ? '-' : '+'}</button>
+      </div>
+        {isOpen && <p className="content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspematur veniam repellendus.</p>}
+      </div>
+    </>
+  );
+}
